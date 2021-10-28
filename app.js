68fc591c7f6352dd056527c8760659d0594530d3
antarjaala <shivaprasadsk@gmail.com>
2021-10-28 22:33:55 +0530

    second data

--- app.js
+++ app.js
@@ -3,27 +3,34 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
 // Static Files
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css',express.static(__dirname + 'public/css'))
-app.use('/js',express.static(__dirname + 'public/js'))
-app.use('/img',express.static(__dirname + 'public/img'))
+app.use(express.static('public'))
+// app.use('/css',express.static(__dirname + 'public/css'))
+// app.use('/img',express.static(__dirname + 'public/img'))
+// app.use('/js',express.static(__dirname + 'public/js'))
 
+// Set views
+app.set('views','./views')
+app.set('view engine','ejs')
 
-// Set 'views' directory for any views
-// being rendered res.render()
-app.set('views', path.join(__dirname, 'views'));
-
-// Set view engine as EJS
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.get('', (req, res)=>
-// {res.sendFile(__dirname + '/views/index.html')}
-{res.render('index',{text:'This is EJS'} )}
-)
-
+// Render webpages
+app.get('',(req,res)=>{res.render('index',{text:'This is EJS'})})
+app.get('/about',(req,res)=>{res.render('about',{text: 'About Page'})})
 
 // Listen on port 3000
 app.listen(port, () =>
   console.info(`Listening on port ${port}`))
+
+
+
+
+
+
+
+
+
+
+
+  // Render html
+  // app.get('',function(req,res){res.sendFile(__dirname + "/views/index.html")})
